b065df9cc55bfb1114a7f4af2d30368a953df0c5
Speak2Erase <lily@nowaffles.com>
2022-09-05 01:55:11 -0700

    Disfunctional map picker

--- src/gui/map_picker.rs
+++ src/gui/map_picker.rs
@@ -1,3 +1,5 @@
+use crate::data::rmxp_structs::rpg::MapInfo;
+
 /// The map picker window.
 /// Displays a list of maps in a tree.
 /// Maps can be double clicked to open them in a map editor.
@@ -19,10 +21,29 @@ impl super::window::Window for MapPicker {
         egui::Window::new("Map Picker").open(open).show(ctx, |ui| {
             egui::ScrollArea::both().show(ui, |ui| {
                 let mapinfos = &mut data_cache.expect("Data Cache not loaded").mapinfos;
+                let mut vec = Vec::from_iter(mapinfos.iter());
 
-                for ele in mapinfos.iter() {
-                    ui.label(&ele.1.name);
-                }
+                vec.sort_by(|a, b| a.1.order.cmp(&b.1.order));
+
+                ui.collapsing("root", |ui| {
+                    let mut map_stack = vec![0];
+                    let mut ui_stack = vec![ui];
+
+                    for (id, map) in vec {
+                        while map_stack.len() > 0
+                            && id
+                                != map_stack
+                                    .last()
+                                    .expect("There should be at least 1 element")
+                        {
+                            map_stack.pop();
+                        }
+                        map_stack.push(*id);
+                        egui::CollapsingHeader::new(&map.name)
+                            .id_source(format!("mapinfo_{}_{}", &map.name, id))
+                            .show(ui_stack.last_mut().unwrap(), |ui| {});
+                    }
+                });
             })
         });
     }
