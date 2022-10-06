f10cecdf525d7ca64ce9ce7c7aa10d3e411966ed
Matthew Lyons <matthew@nowaffles.com>
2022-10-05 23:28:26 -0700

    Add windows ico

---
+++ build.rs
@@ -0,0 +1,26 @@
+// Copyright (C) 2022 Lily Lyons
+//
+// This file is part of Luminol.
+//
+// Luminol is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Luminol is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Luminol.  If not, see <http://www.gnu.org/licenses/>.
+
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/windows-icon.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(not(windows))]
+fn main() {}
