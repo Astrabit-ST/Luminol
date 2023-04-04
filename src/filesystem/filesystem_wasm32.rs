bf652305e2da32f734c22c6b6dfc195a6a111a2c
Lily Lyons <lily@nowaffles.com>
2023-04-03 20:37:30 -0700

    fix(wasm): :bug: Fox some bugs in webassembly
    Migrate filesystem functions to javascript, overall cleaning up code and potentially improving load times?
    
    Fixes issues #8 and #9

--- src/filesystem/filesystem_wasm32.rs
+++ src/filesystem/filesystem_wasm32.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 Lily Lyons
+// Copyright (C) 2023 Lily Lyons
 //
 // This file is part of Luminol.
 //
@@ -14,84 +14,104 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Luminol.  If not, see <http://www.gnu.org/licenses/>.
-#![allow(missing_docs)]
-#![allow(unused_variables)]
-
-use std::cell::RefCell;
-use std::path::{Path, PathBuf};
 
 use async_trait::async_trait;
-use wasm_bindgen_futures::JsFuture;
+use std::cell::RefCell;
+use std::path::{Path, PathBuf};
 
 use crate::data::config::RGSSVer;
 use crate::UpdateInfo;
 
 use wasm_bindgen::prelude::*;
 
-use web_sys::{
-    File, FileSystemDirectoryHandle, FileSystemFileHandle, FileSystemGetDirectoryOptions,
-    FileSystemGetFileOptions, FileSystemHandle, FileSystemWritableFileStream,
-};
-
-#[wasm_bindgen]
+#[wasm_bindgen(module = "/assets/filesystem.js")]
 extern "C" {
-    #[wasm_bindgen(js_class = Window, js_name = showDirectoryPicker)]
-    #[wasm_bindgen(catch)]
-    async fn show_directory_picker(this: &web_sys::Window) -> Result<JsValue, JsValue>;
+    fn js_filesystem_supported() -> bool;
+
+    #[wasm_bindgen(js_name = tryOpenFolder, catch)]
+    async fn js_try_open_folder() -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(js_name = readFile, catch)]
+    async fn js_read_file(path: &str) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(js_name = writeFile, catch)]
+    async fn js_write_file(path: &str, data: js_sys::Uint8Array) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(js_name = dirChildren, catch)]
+    async fn js_dir_children(path: &str) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(js_name = createDir, catch)]
+    async fn js_create_dir(path: &str) -> Result<JsValue, JsValue>;
+}
+
+async fn try_open_folder() -> Result<String, js_sys::Error> {
+    let path = js_try_open_folder()
+        .await
+        .map_err(JsValue::unchecked_into::<js_sys::Error>)?;
+
+    Ok(path.as_string().unwrap())
+}
+
+async fn read_file(path: &str) -> Result<Vec<u8>, js_sys::Error> {
+    let data = js_read_file(path)
+        .await
+        .map_err(JsValue::unchecked_into::<js_sys::Error>)?;
+    let data: js_sys::Uint8Array = data.unchecked_into();
+
+    Ok(data.to_vec())
+}
+
+async fn write_file(path: &str, data: &[u8]) -> Result<(), js_sys::Error> {
+    let buf = js_sys::Uint8Array::from(data);
+    js_write_file(path, buf)
+        .await
+        .map_err(JsValue::unchecked_into::<js_sys::Error>)?;
+
+    Ok(())
+}
+
+async fn dir_children(path: &str) -> Result<Vec<String>, js_sys::Error> {
+    let children = js_dir_children(path)
+        .await
+        .map_err(JsValue::unchecked_into::<js_sys::Error>)?;
+    let children: js_sys::Array = children.unchecked_into();
+
+    Ok(children.iter().map(|v| v.as_string().unwrap()).collect())
+}
+
+async fn create_dir(path: &str) -> Result<(), js_sys::Error> {
+    js_create_dir(path)
+        .await
+        .map_err(JsValue::unchecked_into::<js_sys::Error>)?;
+
+    Ok(())
+}
+
+macro_rules! gaurd_supported {
+    () => {
+        if !js_filesystem_supported() {
+            return Err("Filesystem Access API is not supported in your browser.".to_string());
+        }
+    };
 }
 
 #[derive(Default)]
 pub struct Filesystem {
     project_path: RefCell<Option<PathBuf>>,
     loading_project: RefCell<bool>,
-    directory: RefCell<Option<FileSystemDirectoryHandle>>, // FIXME: Use cell
 }
 
 impl Filesystem {
-    async fn get_file(&self, path: &Path) -> Result<File, js_sys::Error> {
-        let file = self
-            .get_parent_dir(path)
-            .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)?;
-
-        let promise = file.get_file_handle(path.file_name().unwrap().to_str().unwrap())?;
-        let file: FileSystemFileHandle = JsFuture::from(promise).await?.dyn_into()?;
-
-        let promise = FileSystemFileHandle::get_file(&file)?;
-        JsFuture::from(promise)
-            .await?
-            .dyn_into()
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-    }
-
-    async fn get_parent_dir(&self, path: &Path) -> Result<FileSystemDirectoryHandle, JsValue> {
-        let mut file = {
-            let directory = self.directory.borrow(); // FIXME: USE CELL NOT AWAIT
-
-            directory.clone().ok_or("Project not open".to_string())?
-        };
-
-        if let Some(parent) = path.parent() {
-            for p in parent.components() {
-                let p = p.as_os_str();
-                let p = p.to_str().unwrap();
-
-                let promise = file.get_directory_handle(p)?;
-                file = JsFuture::from(promise).await?.dyn_into()?;
-            }
-        }
-
-        Ok(file)
-    }
-
     async fn create_project(
         &self,
         name: String,
+        path: PathBuf,
         info: &'static UpdateInfo,
         rgss_ver: RGSSVer,
     ) -> Result<(), String> {
         use super::filesystem_trait::Filesystem;
 
+        *self.project_path.borrow_mut() = Some(path);
         self.create_directory("").await?;
 
         if !self.dir_children(".").await?.is_empty() {
@@ -155,42 +175,11 @@ impl super::filesystem_trait::Filesystem for Filesystem {
 
     /// Get the directory children of a path.
     async fn dir_children(&self, path: impl AsRef<Path>) -> Result<Vec<String>, String> {
-        let folder = self
-            .get_parent_dir(path.as_ref())
+        let children = dir_children(path.as_ref().to_str().unwrap())
             .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
+            .map_err(|e| e.to_string())?;
 
-        let prefix = path.as_ref().file_name().unwrap();
-        let prefix = prefix.to_str().unwrap();
-        let promise = folder
-            .get_directory_handle(prefix)
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-        let folder: FileSystemDirectoryHandle = JsFuture::from(promise)
-            .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?
-            .unchecked_into();
-
-        let iter = js_sys::try_iter(&folder).unwrap().unwrap();
-        let mut entries = vec![];
-
-        for next in iter {
-            let next: js_sys::Promise = next
-                .map_err(JsValue::unchecked_into::<js_sys::Error>)
-                .map_err(|e| e.to_string().as_string().unwrap())?
-                .unchecked_into();
-            let next: FileSystemHandle = JsFuture::from(next)
-                .await
-                .map_err(JsValue::unchecked_into::<js_sys::Error>)
-                .map_err(|e| e.to_string().as_string().unwrap())?
-                .unchecked_into();
-
-            entries.push(FileSystemHandle::name(&next));
-        }
-
-        Ok(entries)
+        Ok(children)
     }
 
     /// Read a data file and deserialize it with RON (rusty object notation)
@@ -200,64 +189,30 @@ impl super::filesystem_trait::Filesystem for Filesystem {
     where
         T: serde::de::DeserializeOwned,
     {
-        let buf = self.read_bytes(path).await?;
+        let data = self.read_bytes(path).await?;
+        let data = alox_48::from_bytes(&data).map_err(|e| e.to_string())?;
 
-        alox_48::from_bytes(&buf).map_err(|e| e.to_string())
+        Ok(data)
     }
 
     /// Read bytes from a file.
     async fn read_bytes(&self, path: impl AsRef<Path>) -> Result<Vec<u8>, String> {
-        let file = self
-            .get_file(path.as_ref())
+        let data = read_file(path.as_ref().to_str().unwrap())
             .await
-            .map_err(|e| e.to_string().as_string().unwrap())?;
+            .map_err(|e| e.to_string())?;
 
-        let buf = JsFuture::from(file.array_buffer());
-        let buf = buf
-            .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-        let buf: js_sys::ArrayBuffer = buf.unchecked_into();
-        let buf = js_sys::Uint8Array::new(&buf);
-
-        Ok(buf.to_vec())
+        Ok(data)
     }
 
-    /// Save some file's data by serializing it with RON.
+    /// Save some bytes to a file.
     async fn save_data(
         &self,
         path: impl AsRef<Path>,
         data: impl AsRef<[u8]>,
     ) -> Result<(), String> {
-        let folder = self.get_parent_dir(path.as_ref()).await.unwrap();
-
-        let mut options = FileSystemGetFileOptions::new();
-        options.create(true);
-        let promise = folder
-            .get_file_handle_with_options(
-                path.as_ref().file_name().unwrap().to_str().unwrap(),
-                &options,
-            )
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-
-        let file: FileSystemFileHandle = JsFuture::from(promise).await.unwrap().unchecked_into();
-        let promise = file
-            .create_writable()
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-
-        let stream: FileSystemWritableFileStream = JsFuture::from(promise)
+        write_file(path.as_ref().to_str().unwrap(), data.as_ref())
             .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?
-            .unchecked_into();
-
-        let promise = stream.write_with_buffer_source(&js_sys::Uint8Array::from(data.as_ref()));
-        JsFuture::from(promise)
-            .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
+            .map_err(|e| e.to_string())?;
 
         Ok(())
     }
@@ -286,57 +241,28 @@ impl super::filesystem_trait::Filesystem for Filesystem {
 
     /// Try to open a project.
     async fn try_open_project(&self, info: &'static UpdateInfo) -> Result<(), String> {
-        let window = web_sys::window().unwrap();
+        gaurd_supported!();
 
-        let directory = show_directory_picker(&window)
-            .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-        let directory: FileSystemDirectoryHandle = directory
-            .dyn_into()
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-
-        *self.project_path.borrow_mut() = Some(directory.name().into());
-        *self.directory.borrow_mut() = Some(directory);
-
-        *self.loading_project.borrow_mut() = true;
+        let path = try_open_folder().await.map_err(|e| e.to_string())?;
+        self.project_path.replace(Some(PathBuf::from(path)));
+        self.loading_project.replace(true);
 
         info.data_cache.load(self).await.map_err(|e| {
-            *self.project_path.borrow_mut() = None;
-            *self.directory.borrow_mut() = None;
-
+            self.project_path.replace(None);
+            self.loading_project.replace(false);
             e
         })?;
 
-        *self.loading_project.borrow_mut() = false;
+        self.loading_project.replace(false);
 
         Ok(())
     }
 
     /// Create a directory at the specified path.
     async fn create_directory(&self, path: impl AsRef<Path>) -> Result<(), String> {
-        let dir = self
-            .get_parent_dir(path.as_ref())
+        create_dir(path.as_ref().to_str().unwrap())
             .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-
-        let mut options = FileSystemGetDirectoryOptions::new();
-        options.create(true);
-
-        let promise = dir
-            .get_directory_handle_with_options(
-                path.as_ref().file_name().unwrap().to_str().unwrap(),
-                &options,
-            )
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-
-        JsFuture::from(promise)
-            .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
+            .map_err(|e| e.to_string())?;
 
         Ok(())
     }
@@ -348,25 +274,17 @@ impl super::filesystem_trait::Filesystem for Filesystem {
         info: &'static UpdateInfo,
         rgss_ver: RGSSVer,
     ) -> Result<(), String> {
-        let window = web_sys::window().unwrap();
+        gaurd_supported!();
 
-        let directory = show_directory_picker(&window)
+        let path = try_open_folder().await.map_err(|e| e.to_string())?;
+
+        self.create_project(name, path.into(), info, rgss_ver)
             .await
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-        let directory: FileSystemDirectoryHandle = directory
-            .dyn_into()
-            .map_err(JsValue::unchecked_into::<js_sys::Error>)
-            .map_err(|e| e.to_string().as_string().unwrap())?;
-
-        *self.project_path.borrow_mut() = Some(directory.name().into());
-        *self.directory.borrow_mut() = Some(directory);
-
-        *self.loading_project.borrow_mut() = true;
-
-        self.create_project(name, info, rgss_ver).await?;
-
-        *self.loading_project.borrow_mut() = false;
+            .map_err(|e| {
+                self.project_path.replace(None);
+                self.loading_project.replace(false);
+                e
+            })?;
 
         Ok(())
     }
