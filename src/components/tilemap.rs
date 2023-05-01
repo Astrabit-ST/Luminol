6f3692d2012f2feb4df19dbfeb855901745f24a4
Speak2Erase <lily@nowaffles.com>
2023-05-01 15:25:19 -0700

    feat(tilemap): :construction: Hasty commit so I can work on my other computer

--- src/components/tilemap.rs
+++ src/components/tilemap.rs
@@ -71,56 +71,22 @@ macro_rules! check_for_gl_error {
 }
 
 static TILEMAP_SHADER: once_cell::sync::Lazy<glow::Program> =
-    once_cell::sync::Lazy::new(|| unsafe {
-        let gl = &state!().gl;
+    once_cell::sync::Lazy::new(|| unsafe { create_tilemap_shader() });
 
-        let vertex = gl
-            .create_shader(glow::VERTEX_SHADER)
-            .expect("failed to create shader");
-        gl.shader_source(vertex, include_str!("tilemap.vert"));
-        gl.compile_shader(vertex);
+const MAX_SIZE: i32 = 8192; // Max texture size in one dimension
+const TILE_SIZE: i32 = 32; // Tiles are 32x32
+const TILESET_WIDTH: i32 = TILE_SIZE * 8; // Tilesets are 8 tiles across
 
-        if !gl.get_shader_compile_status(vertex) {
-            println!("failed to compile tilemap vertex shader");
-            println!("{}", gl.get_shader_info_log(vertex));
-            std::process::exit(1);
-        }
-
-        let fragment = gl
-            .create_shader(glow::FRAGMENT_SHADER)
-            .expect("failed to create shader");
-        gl.shader_source(fragment, include_str!("tilemap.frag"));
-        gl.compile_shader(fragment);
-
-        if !gl.get_shader_compile_status(fragment) {
-            println!("failed to compile tilemap frag shader");
-            println!("{}", gl.get_shader_info_log(fragment));
-            std::process::exit(1);
-        }
-
-        let program = gl
-            .create_program()
-            .expect("failed to create shader program");
-
-        gl.attach_shader(program, vertex);
-        gl.attach_shader(program, fragment);
-        gl.link_program(program);
-
-        if !gl.get_program_completion_status(program) {
-            println!("failed to link tilemap shader");
-            println!("{}", gl.get_program_info_log(program));
-            std::process::exit(1);
-        }
-
-        gl.delete_shader(vertex);
-        gl.delete_shader(fragment);
-
-        program
-    });
+const ANIM_FRAME_COUNT: i32 = 4; // Autotiles have 4 frames of animation
+const AUTOTILE_WIDTH: i32 = TILE_SIZE * 3 * ANIM_FRAME_COUNT; // Each frame is 3 tiles wide
+const AUTOTILE_HEIGHT: i32 = TILE_SIZE * 4; // Autotiles are 4 tiles high
+const AUTOTILE_AMOUNT: i32 = 7; // There are 7 autotiles per tileset
+const TOTAL_AUTOTILE_HEIGHT: i32 = AUTOTILE_HEIGHT * AUTOTILE_AMOUNT;
 
 impl Tilemap {
     pub fn new(id: i32) -> Result<Tilemap, String> {
         let textures = Arc::new(Self::load_data(id)?);
+        check_for_gl_error!("generating textures");
 
         let gl = &state!().gl;
 
@@ -136,16 +102,54 @@ impl Tilemap {
             gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
             gl.buffer_data_u8_slice(
                 glow::ARRAY_BUFFER,
-                bytemuck::cast_slice(&[-0.5_f32, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0]),
+                bytemuck::cast_slice::<f32, u8>(&[
+                    0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, // top right
+                    0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, // bottom right
+                    -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, // bottom left
+                    -0.5, 0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, // top left
+                ]),
                 glow::STATIC_DRAW,
             );
 
-            gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, 3 * size_of::<f32>() as i32, 0);
+            gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, 8 * size_of::<f32>() as i32, 0);
             gl.enable_vertex_attrib_array(0);
+            gl.vertex_attrib_pointer_f32(
+                1,
+                3,
+                glow::FLOAT,
+                false,
+                8 * size_of::<f32>() as i32,
+                3 * size_of::<f32>() as i32,
+            );
+            gl.enable_vertex_attrib_array(1);
+            gl.vertex_attrib_pointer_f32(
+                2,
+                2,
+                glow::FLOAT,
+                false,
+                8 * size_of::<f32>() as i32,
+                6 * size_of::<f32>() as i32,
+            );
+            gl.enable_vertex_attrib_array(2);
 
             vbo
         };
         check_for_gl_error!("generating vbo");
+
+        let ebo = unsafe {
+            let ebo = gl.create_buffer().expect("failed to create ebo");
+            gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
+            gl.buffer_data_u8_slice(
+                glow::ELEMENT_ARRAY_BUFFER,
+                bytemuck::cast_slice::<u32, u8>(&[
+                    0, 1, 3, // first triangle
+                    1, 2, 3, // second triangle
+                ]),
+                glow::STATIC_DRAW,
+            );
+
+            ebo
+        };
 
         Ok(Self {
             pan: egui::Vec2::ZERO,
@@ -174,6 +178,7 @@ impl Tilemap {
         ui.set_clip_rect(canvas_rect);
 
         let vao = self.vao;
+        let atlas = unsafe { self.textures.atlas.raw() };
         ui.painter().add(egui::PaintCallback {
             rect: canvas_rect,
             callback: Arc::new(eframe::egui_glow::CallbackFn::new(move |_i, painter| {
@@ -183,8 +188,11 @@ impl Tilemap {
                     gl.clear(glow::COLOR_BUFFER_BIT);
 
                     gl.bind_vertex_array(Some(vao));
+
+                    gl.active_texture(glow::TEXTURE0);
+                    gl.bind_texture(glow::TEXTURE_2D, Some(atlas));
                     gl.use_program(Some(*TILEMAP_SHADER));
-                    gl.draw_arrays(glow::TRIANGLES, 0, 3);
+                    gl.draw_elements(glow::TRIANGLES, 6, glow::UNSIGNED_INT, 0);
 
                     check_for_gl_error!("painting tilemap");
                 }
@@ -265,47 +273,23 @@ impl Tilemap {
         tileset: &rpg::Tileset,
         tileset_height: i32,
     ) -> Result<(i32, i32), String> {
-        // Limit the size of the tileset to 8192
-        const maxAtlasSize: i32 = 8192;
-        const atBuffer: i32 = 32;
-        /* Autotile area width */
-        const atAreaW: i32 = 32 * 3 * 8;
-        /* Autotile area height */
-        const atAreaH: i32 = 32 * 4 * 7 + atBuffer;
-        const tilesetW: i32 = 32 * 8;
-        const tsLaneW: i32 = tilesetW / 1;
-        const underAtLanes: i32 = atAreaW / tsLaneW + !!(atAreaW % tsLaneW);
+        let mut width = AUTOTILE_WIDTH;
+        let mut height = TOTAL_AUTOTILE_HEIGHT;
+        println!("initial size {width}x{height}");
+        height += tileset_height;
+        println!("tilemap + initial size {width}x{height}");
 
-        /* Autotile area */
-        const atArea: i32 = underAtLanes * tsLaneW * atAreaH;
-
-        let mut width = underAtLanes * tsLaneW;
-        let mut height = atAreaH;
-        let tsArea = tilesetW * tileset_height;
-
-        fn freeArea(width: i32, height: i32) -> i32 {
-            width * height - atArea
+        while height > MAX_SIZE {
+            width += TILESET_WIDTH;
+            height -= height % 8192;
+            println!("resizing to {width}x{height}");
         }
 
-        /* Expand vertically */
-        while freeArea(width, height) < tsArea && height < maxAtlasSize {
-            height += 32;
+        if width > MAX_SIZE || height > MAX_SIZE {
+            Err("cannot fit tileset into an 8192x8192 texture".to_string())
+        } else {
+            Ok((width, height))
         }
-
-        if freeArea(width, height) >= tsArea && height <= maxAtlasSize {
-            return Ok((width, height));
-        }
-
-        /* Expand horizontally */
-        while freeArea(width, height) < tsArea && width < maxAtlasSize {
-            width += tsLaneW;
-        }
-
-        if (freeArea(width, height) >= tsArea && width <= maxAtlasSize) {
-            return Ok((width, height));
-        }
-
-        Err("cannot fit tileset into an 8192x8192 texture".to_string())
     }
 
     fn build_atlas(tileset: &rpg::Tileset) -> Result<GlTexture, String> {
@@ -318,12 +302,12 @@ impl Tilemap {
             let (width, height) =
                 Self::calc_atlas_dimensions(tileset, tileset_img.height() as i32)?;
 
-            let texture = gl.create_texture()?;
-            gl.bind_texture(glow::TEXTURE_2D, Some(texture));
+            let atlas = gl.create_texture()?;
+            gl.bind_texture(glow::TEXTURE_2D, Some(atlas));
             gl.tex_image_2d(
                 glow::TEXTURE_2D,
                 0,
-                glow::RGBA as i32,
+                glow::RGBA as _,
                 width,
                 height,
                 0,
@@ -331,13 +315,109 @@ impl Tilemap {
                 glow::UNSIGNED_BYTE,
                 None,
             );
+            gl.generate_mipmap(glow::TEXTURE_2D);
+            check_for_gl_error!("creating tileset atlas");
 
-            Ok(GlTexture::new(texture, width as u32, height as u32))
+            let fbo = gl
+                .create_framebuffer()
+                .expect("failed to create framebuffer");
+            gl.bind_framebuffer(glow::FRAMEBUFFER, Some(fbo));
+            gl.framebuffer_texture_2d(
+                glow::DRAW_FRAMEBUFFER,
+                glow::COLOR_ATTACHMENT1,
+                glow::TEXTURE_2D,
+                Some(atlas),
+                0,
+            );
+
+            for (index, tile) in tileset.autotile_names.iter().enumerate() {
+                if tile.is_empty() {
+                    continue;
+                }
+                let autotile_tex = state!()
+                    .image_cache
+                    .load_glow_image("Graphics/Autotiles", tile)?;
+                println!("tex: {autotile_tex:?}");
+
+                gl.framebuffer_texture_2d(
+                    glow::READ_FRAMEBUFFER,
+                    glow::COLOR_ATTACHMENT0,
+                    glow::TEXTURE_2D,
+                    Some(autotile_tex.raw()),
+                    0,
+                );
+                gl.draw_buffer(glow::COLOR_ATTACHMENT1);
+                gl.blit_framebuffer(
+                    0,
+                    0,
+                    autotile_tex.width() as i32,
+                    AUTOTILE_HEIGHT,
+                    0,
+                    0,
+                    autotile_tex.width() as i32,
+                    AUTOTILE_HEIGHT,
+                    glow::COLOR_BUFFER_BIT,
+                    glow::NEAREST,
+                );
+                check_for_gl_error!("copying autotile to atlas");
+            }
+
+            gl.bind_framebuffer(glow::FRAMEBUFFER, None);
+            gl.delete_framebuffer(fbo);
+
+            Ok(GlTexture::new(atlas, width as u32, height as u32))
         }
     }
 }
 
-pub fn check_for_gl_error_impl(file: &str, line: u32, context: &str) {
+unsafe fn create_tilemap_shader() -> glow::Program {
+    let gl = &state!().gl;
+
+    let vertex = gl
+        .create_shader(glow::VERTEX_SHADER)
+        .expect("failed to create shader");
+    gl.shader_source(vertex, include_str!("tilemap.vert"));
+    gl.compile_shader(vertex);
+
+    if !gl.get_shader_compile_status(vertex) {
+        println!("failed to compile tilemap vertex shader");
+        println!("{}", gl.get_shader_info_log(vertex));
+        std::process::exit(1);
+    }
+
+    let fragment = gl
+        .create_shader(glow::FRAGMENT_SHADER)
+        .expect("failed to create shader");
+    gl.shader_source(fragment, include_str!("tilemap.frag"));
+    gl.compile_shader(fragment);
+
+    if !gl.get_shader_compile_status(fragment) {
+        println!("failed to compile tilemap frag shader");
+        println!("{}", gl.get_shader_info_log(fragment));
+        std::process::exit(1);
+    }
+
+    let program = gl
+        .create_program()
+        .expect("failed to create shader program");
+
+    gl.attach_shader(program, vertex);
+    gl.attach_shader(program, fragment);
+    gl.link_program(program);
+
+    if !gl.get_program_completion_status(program) {
+        println!("failed to link tilemap shader");
+        println!("{}", gl.get_program_info_log(program));
+        std::process::exit(1);
+    }
+
+    gl.delete_shader(vertex);
+    gl.delete_shader(fragment);
+
+    program
+}
+
+fn check_for_gl_error_impl(file: &str, line: u32, context: &str) {
     let gl = &state!().gl;
     #[allow(unsafe_code)]
     let error_code = unsafe { gl.get_error() };
