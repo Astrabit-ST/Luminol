b503fd6811dfc309cd64dabb91ee2bcce2f601de
white-axe <whiteaxe@tuta.io>
2023-10-10 22:39:54 -0400

    Move `skip` variable from previous commit outside of the loop

--- src/filesystem/path_cache.rs
+++ src/filesystem/path_cache.rs
@@ -51,14 +51,13 @@ where
                 path: impl AsRef<camino::Utf8Path>,
                 f: &mut impl FnMut(&camino::Utf8Path),
             ) -> Result<(), Error> {
+                // In web builds, RTPs are currently to be placed in the "RTP" subdirectory of
+                // the project root directory, so this is to avoid loading the contents of
+                // those directories twice
+                let skip = matches!(path.as_ref().iter().next_back(), Some("RTP"));
+
                 for entry in fs.read_dir(path)? {
                     f(entry.path());
-
-                    // In web builds, RTPs are currently to be placed in the "RTP" subdirectory of
-                    // the project root directory, so this is to avoid loading the contents of
-                    // those directories twice
-                    let skip = matches!(entry.path().iter().nth_back(1), Some("RTP"));
-
                     if !skip && !entry.metadata().is_file {
                         internal(fs, entry.path(), f)?;
                     }
