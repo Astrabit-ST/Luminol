5591e74fad89dddf4bd6af4ec77d2d481333ec48
white-axe <whiteaxe@tuta.io>
2023-11-22 16:56:12 -0500

    Make the code from the previous commit less terrible
    (cherry picked from commit 1b968cad8943859b332f93ce3755d32590f46d9b)

--- crates/core/src/tab.rs
+++ crates/core/src/tab.rs
@@ -145,16 +145,10 @@ impl Tabs {
                             is_empty = false;
                         }
 
-                        // Check if the originally focused tab is inside this tree
-                        if let Some(k) = focused_id.and_then(|id| {
-                            tabs.iter().enumerate().find_map(|(i, tab)| {
-                                if tab.id() == id {
-                                    Some(i)
-                                } else {
-                                    None
-                                }
-                            })
-                        }) {
+                        // Check if the originally focused tab is inside this node
+                        if let Some((k, _)) = focused_id
+                            .and_then(|id| tabs.iter().enumerate().find(|(_, tab)| tab.id() == id))
+                        {
                             active_tab = Some((i, j, k));
                         }
                     }
