3680922bad80d54a2258ae03033f46b56e3d8bf3
white-axe <whiteaxe@tuta.io>
2023-11-22 14:34:41 -0500

    Replace image cache with dummy bind group in collision shader
    (cherry picked from commit cbbf696d102af03d83edcd9a50ac0ce9645181c9)

--- crates/graphics/src/collision/shader.rs
+++ crates/graphics/src/collision/shader.rs
@@ -60,7 +60,7 @@ pub fn create_render_pipeline(
             .device
             .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Tilemap Collision Render Pipeline Layout (push constants)"),
-                bind_group_layouts: &[&bind_group_layouts.image_cache_texture],
+                bind_group_layouts: &[&bind_group_layouts.dummy],
                 push_constant_ranges: &[
                     // Viewport
                     wgpu::PushConstantRange {
@@ -74,10 +74,7 @@ pub fn create_render_pipeline(
             .device
             .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Tilemap Collision Render Pipeline Layout (uniforms)"),
-                bind_group_layouts: &[
-                    &bind_group_layouts.image_cache_texture,
-                    &bind_group_layouts.viewport,
-                ],
+                bind_group_layouts: &[&bind_group_layouts.dummy, &bind_group_layouts.viewport],
                 push_constant_ranges: &[],
             })
     };
