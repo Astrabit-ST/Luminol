a118a9366bde6a509723ce73160b1a306664aafa
Speak2Erase <lily@nowaffles.com>
2024-03-22 22:45:14 -0700

    Use a VecDeque for the log window buffer

--- src/app/log_window.rs
+++ src/app/log_window.rs
@@ -22,20 +22,20 @@
 // terms of the Steamworks API by Valve Corporation, the licensors of this
 // Program grant you additional permission to convey the resulting work.
 
-use std::sync::mpsc::{Receiver, Sender};
-
-use ringbuf::{ring_buffer::RbBase, Rb};
+use std::{
+    collections::VecDeque,
+    sync::mpsc::{Receiver, Sender},
+};
 
 pub struct LogWindow {
     pub(super) term_shown: bool,
     byte_rx: Receiver<u8>,
     byte_tx: Sender<u8>,
-    ringbuf: RingBuf, // Should we use a temporary file instead of a ringbuffer?
+    buffer: VecDeque<u8>,
     term: luminol_term::widget::ChannelTerminal,
     save_promise: Option<poll_promise::Promise<luminol_filesystem::Result<()>>>,
 }
 const MAX_BUFFER_CAPACITY: usize = 1 << 24;
-type RingBuf = ringbuf::LocalRb<u8, Vec<std::mem::MaybeUninit<u8>>>;
 
 impl LogWindow {
     pub fn new(
@@ -44,12 +44,11 @@ impl LogWindow {
     ) -> Self {
         let (byte_tx, term_byte_rx) = std::sync::mpsc::channel();
         let term = luminol_term::widget::Terminal::channel(term_byte_rx, config);
-        let ringbuf = RingBuf::new(2 << 12);
 
         Self {
             byte_rx,
             byte_tx,
-            ringbuf,
+            buffer: VecDeque::new(),
             term,
             term_shown: false,
             save_promise: None,
@@ -61,18 +60,12 @@ impl LogWindow {
         for byte in self.byte_rx.try_iter() {
             let _ = self.byte_tx.send(byte);
 
-            // resize ringbuffer if it is full
-            let capacity = self.ringbuf.capacity();
-            if self.ringbuf.is_full() && capacity < MAX_BUFFER_CAPACITY {
-                let mut new_ringbuf = RingBuf::new(capacity << 1);
-
-                let (front, back) = self.ringbuf.as_slices();
-                new_ringbuf.push_slice(front);
-                new_ringbuf.push_slice(back);
-
-                self.ringbuf = new_ringbuf;
+            if self.buffer.len() >= MAX_BUFFER_CAPACITY {
+                self.buffer.pop_front();
+                self.buffer.push_back(byte);
+            } else {
+                self.buffer.push_back(byte);
             }
-            self.ringbuf.push_overwrite(byte);
 
             did_recv = true;
         }
@@ -93,7 +86,7 @@ impl LogWindow {
                     ui.add_space(ui.style().spacing.indent);
 
                     if ui.button("Clear").clicked() {
-                        self.ringbuf.clear();
+                        self.buffer.clear();
                         self.term.erase_scrollback_and_viewport();
                     }
 
@@ -120,9 +113,7 @@ impl LogWindow {
                             Err(p) => self.save_promise = Some(p),
                         }
                     } else if ui.button("Save to file").clicked() {
-                        let (left, right) = self.ringbuf.as_slices();
-                        let mut buffer = left.to_vec();
-                        buffer.extend_from_slice(right);
+                        let buffer = self.buffer.make_contiguous().to_vec();
 
                         self.save_promise =
                             Some(luminol_core::spawn_future(Self::save_promise(buffer)));
