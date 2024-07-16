5555362cea93bae9587be35f9855ee6a9e385892
Melody Madeline Lyons <lily@nowaffles.com>
2024-07-15 19:37:13 -0700

    Data conversion (#141)
    * Conversion that sort of works
    
    * Refactor code to be more async
    
    * Fix wasm compile error
    
    * Oops
    
    * Actually deserialize/serialize parameter type now
    
    * Add pretty print support
    
    * Flush files

--- crates/core/src/data_cache/data_formats.rs
+++ crates/core/src/data_cache/data_formats.rs
@@ -36,6 +36,12 @@ impl Handler {
         Self { format }
     }
 
+    pub fn path_for(self, filename: impl AsRef<camino::Utf8Path>) -> camino::Utf8PathBuf {
+        camino::Utf8Path::new("Data")
+            .join(filename)
+            .with_extension(self.format.extension())
+    }
+
     pub fn read_data<T>(
         self,
         filesystem: &impl luminol_filesystem::FileSystem,
@@ -45,10 +51,7 @@ impl Handler {
         T: for<'de> alox_48::Deserialize<'de>,
         T: ::serde::de::DeserializeOwned,
     {
-        let path = camino::Utf8Path::new("Data")
-            .join(filename)
-            .with_extension(self.format.extension());
-        let data = filesystem.read(path)?;
+        let data = filesystem.read(self.path_for(filename))?;
 
         match self.format {
             DataFormat::Marshal => {
@@ -57,12 +60,35 @@ impl Handler {
 
                 result.map_err(|(error, trace)| format_traced_error(error, trace))
             }
-            DataFormat::Ron => {
+            DataFormat::Ron { .. } => {
                 let mut de = ron::de::Deserializer::from_bytes(&data)?;
                 serde_path_to_error::deserialize(&mut de).map_err(format_path_to_error)
             }
-            DataFormat::Json => {
+            DataFormat::Json { .. } => {
                 let mut de = serde_json::de::Deserializer::from_slice(&data);
+                serde_path_to_error::deserialize(&mut de).map_err(format_path_to_error)
+            }
+        }
+    }
+
+    pub fn read_data_from<T>(self, data: &[u8]) -> color_eyre::Result<T>
+    where
+        T: for<'de> alox_48::Deserialize<'de>,
+        T: ::serde::de::DeserializeOwned,
+    {
+        match self.format {
+            DataFormat::Marshal => {
+                let mut de = alox_48::Deserializer::new(data)?;
+                let result = alox_48::path_to_error::deserialize(&mut de);
+
+                result.map_err(|(error, trace)| format_traced_error(error, trace))
+            }
+            DataFormat::Ron { .. } => {
+                let mut de = ron::de::Deserializer::from_bytes(data)?;
+                serde_path_to_error::deserialize(&mut de).map_err(format_path_to_error)
+            }
+            DataFormat::Json { .. } => {
+                let mut de = serde_json::de::Deserializer::from_slice(data);
                 serde_path_to_error::deserialize(&mut de).map_err(format_path_to_error)
             }
         }
@@ -78,11 +104,8 @@ impl Handler {
         T: ::serde::Serialize,
         T: alox_48::Serialize,
     {
-        let path = camino::Utf8Path::new("Data")
-            .join(filename)
-            .with_extension(self.format.extension());
         let mut file = filesystem.open_file(
-            path,
+            self.path_for(filename),
             OpenFlags::Create | OpenFlags::Truncate | OpenFlags::Write,
         )?;
 
@@ -93,13 +116,52 @@ impl Handler {
                     .map_err(|(error, trace)| format_traced_error(error, trace))?;
                 file.write_all(&serializer.output)?;
             }
-            DataFormat::Ron => {
-                let mut ser = ron::Serializer::new(&mut file, None)?;
+            DataFormat::Ron { pretty } => {
+                let config = pretty.then(|| ron::ser::PrettyConfig::new().struct_names(true));
+                let mut ser =
+                    ron::Serializer::with_options(&mut file, config, ron::Options::default())?;
                 serde_path_to_error::serialize(data, &mut ser)?;
             }
-            DataFormat::Json => {
-                let mut ser = serde_json::Serializer::new(&mut file);
+            DataFormat::Json { pretty } => {
+                if pretty {
+                    let mut ser = serde_json::Serializer::pretty(&mut file);
+                    serde_path_to_error::serialize(data, &mut ser)?;
+                } else {
+                    let mut ser = serde_json::Serializer::new(&mut file);
+                    serde_path_to_error::serialize(data, &mut ser)?;
+                }
+            }
+        };
+
+        Ok(())
+    }
+
+    pub fn write_data_to<T>(self, data: &T, buffer: &mut Vec<u8>) -> color_eyre::Result<()>
+    where
+        T: ::serde::Serialize,
+        T: alox_48::Serialize,
+    {
+        match self.format {
+            DataFormat::Marshal => {
+                let mut serializer = alox_48::Serializer::new();
+                alox_48::path_to_error::serialize(data, &mut serializer)
+                    .map_err(|(error, trace)| format_traced_error(error, trace))?;
+                buffer.extend_from_slice(&serializer.output);
+            }
+            DataFormat::Ron { pretty } => {
+                let config = pretty.then(|| ron::ser::PrettyConfig::new().struct_names(true));
+                let mut ser =
+                    ron::Serializer::with_options(buffer, config, ron::Options::default())?;
                 serde_path_to_error::serialize(data, &mut ser)?;
+            }
+            DataFormat::Json { pretty } => {
+                if pretty {
+                    let mut ser = serde_json::Serializer::pretty(buffer);
+                    serde_path_to_error::serialize(data, &mut ser)?;
+                } else {
+                    let mut ser = serde_json::Serializer::new(buffer);
+                    serde_path_to_error::serialize(data, &mut ser)?;
+                }
             }
         };
 
@@ -115,10 +177,7 @@ impl Handler {
         T: for<'de> alox_48::Deserialize<'de>,
         T: ::serde::de::DeserializeOwned,
     {
-        let path = camino::Utf8Path::new("Data")
-            .join(filename)
-            .with_extension(self.format.extension());
-        let data = filesystem.read(path)?;
+        let data = filesystem.read(self.path_for(filename))?;
 
         match self.format {
             DataFormat::Marshal => {
@@ -129,7 +188,7 @@ impl Handler {
                 luminol_data::helpers::nil_padded_alox::deserialize_with(de)
                     .map_err(|error| format_traced_error(error, trace))
             }
-            DataFormat::Ron => {
+            DataFormat::Ron { .. } => {
                 let mut de = ron::de::Deserializer::from_bytes(&data)?;
                 let mut track = serde_path_to_error::Track::new();
                 let de = serde_path_to_error::Deserializer::new(&mut de, &mut track);
@@ -139,8 +198,45 @@ impl Handler {
                     format_path_to_error(error)
                 })
             }
-            DataFormat::Json => {
+            DataFormat::Json { .. } => {
                 let mut de = serde_json::de::Deserializer::from_slice(&data);
+                let mut track = serde_path_to_error::Track::new();
+                let de = serde_path_to_error::Deserializer::new(&mut de, &mut track);
+
+                luminol_data::helpers::nil_padded_serde::deserialize(de).map_err(|inner| {
+                    let error = serde_path_to_error::Error::new(track.path(), inner);
+                    format_path_to_error(error)
+                })
+            }
+        }
+    }
+
+    pub fn read_nil_padded_from<T>(self, data: &[u8]) -> color_eyre::Result<Vec<T>>
+    where
+        T: for<'de> alox_48::Deserialize<'de>,
+        T: ::serde::de::DeserializeOwned,
+    {
+        match self.format {
+            DataFormat::Marshal => {
+                let mut de = alox_48::Deserializer::new(data)?;
+                let mut trace = alox_48::path_to_error::Trace::default();
+                let de = alox_48::path_to_error::Deserializer::new(&mut de, &mut trace);
+
+                luminol_data::helpers::nil_padded_alox::deserialize_with(de)
+                    .map_err(|error| format_traced_error(error, trace))
+            }
+            DataFormat::Ron { .. } => {
+                let mut de = ron::de::Deserializer::from_bytes(data)?;
+                let mut track = serde_path_to_error::Track::new();
+                let de = serde_path_to_error::Deserializer::new(&mut de, &mut track);
+
+                luminol_data::helpers::nil_padded_serde::deserialize(de).map_err(|inner| {
+                    let error = serde_path_to_error::Error::new(track.path(), inner);
+                    format_path_to_error(error)
+                })
+            }
+            DataFormat::Json { .. } => {
+                let mut de = serde_json::de::Deserializer::from_slice(data);
                 let mut track = serde_path_to_error::Track::new();
                 let de = serde_path_to_error::Deserializer::new(&mut de, &mut track);
 
@@ -162,11 +258,8 @@ impl Handler {
         T: ::serde::Serialize,
         T: alox_48::Serialize,
     {
-        let path = camino::Utf8Path::new("Data")
-            .join(filename)
-            .with_extension(self.format.extension());
         let mut file = filesystem.open_file(
-            path,
+            self.path_for(filename),
             OpenFlags::Create | OpenFlags::Truncate | OpenFlags::Write,
         )?;
 
@@ -180,19 +273,35 @@ impl Handler {
                     .map_err(|error| format_traced_error(error, trace))?;
                 file.write_all(&ser.output)?;
             }
-            DataFormat::Json => {
+            DataFormat::Json { pretty } => {
                 let mut track = serde_path_to_error::Track::new();
-                let mut ser = serde_json::Serializer::new(&mut file);
-                let ser = serde_path_to_error::Serializer::new(&mut ser, &mut track);
+                if pretty {
+                    let mut ser = serde_json::Serializer::pretty(&mut file);
+                    let ser = serde_path_to_error::Serializer::new(&mut ser, &mut track);
 
-                luminol_data::helpers::nil_padded_serde::serialize(data, ser).map_err(|inner| {
-                    let error = serde_path_to_error::Error::new(track.path(), inner);
-                    format_path_to_error(error)
-                })?;
+                    luminol_data::helpers::nil_padded_serde::serialize(data, ser).map_err(
+                        |inner| {
+                            let error = serde_path_to_error::Error::new(track.path(), inner);
+                            format_path_to_error(error)
+                        },
+                    )?;
+                } else {
+                    let mut ser = serde_json::Serializer::new(&mut file);
+                    let ser = serde_path_to_error::Serializer::new(&mut ser, &mut track);
+
+                    luminol_data::helpers::nil_padded_serde::serialize(data, ser).map_err(
+                        |inner| {
+                            let error = serde_path_to_error::Error::new(track.path(), inner);
+                            format_path_to_error(error)
+                        },
+                    )?;
+                }
             }
-            DataFormat::Ron => {
+            DataFormat::Ron { pretty } => {
                 let mut track = serde_path_to_error::Track::new();
-                let mut ser = ron::Serializer::new(&mut file, None)?;
+                let config = pretty.then(|| ron::ser::PrettyConfig::new().struct_names(true));
+                let mut ser =
+                    ron::Serializer::with_options(&mut file, config, ron::Options::default())?;
                 let ser = serde_path_to_error::Serializer::new(&mut ser, &mut track);
 
                 luminol_data::helpers::nil_padded_serde::serialize(data, ser).map_err(|inner| {
@@ -203,6 +312,62 @@ impl Handler {
         }
 
         file.flush()?;
+
+        Ok(())
+    }
+
+    pub fn write_nil_padded_to<T>(self, data: &[T], buffer: &mut Vec<u8>) -> color_eyre::Result<()>
+    where
+        T: ::serde::Serialize,
+        T: alox_48::Serialize,
+    {
+        match self.format {
+            DataFormat::Marshal => {
+                let mut trace = alox_48::path_to_error::Trace::new();
+                let mut ser = alox_48::Serializer::new();
+                let trace_ser = alox_48::path_to_error::Serializer::new(&mut ser, &mut trace);
+
+                luminol_data::helpers::nil_padded_alox::serialize_with(data, trace_ser)
+                    .map_err(|error| format_traced_error(error, trace))?;
+                buffer.extend_from_slice(&ser.output);
+            }
+            DataFormat::Json { pretty } => {
+                let mut track = serde_path_to_error::Track::new();
+                if pretty {
+                    let mut ser = serde_json::Serializer::pretty(buffer);
+                    let ser = serde_path_to_error::Serializer::new(&mut ser, &mut track);
+
+                    luminol_data::helpers::nil_padded_serde::serialize(data, ser).map_err(
+                        |inner| {
+                            let error = serde_path_to_error::Error::new(track.path(), inner);
+                            format_path_to_error(error)
+                        },
+                    )?;
+                } else {
+                    let mut ser = serde_json::Serializer::new(buffer);
+                    let ser = serde_path_to_error::Serializer::new(&mut ser, &mut track);
+
+                    luminol_data::helpers::nil_padded_serde::serialize(data, ser).map_err(
+                        |inner| {
+                            let error = serde_path_to_error::Error::new(track.path(), inner);
+                            format_path_to_error(error)
+                        },
+                    )?;
+                }
+            }
+            DataFormat::Ron { pretty } => {
+                let mut track = serde_path_to_error::Track::new();
+                let config = pretty.then(|| ron::ser::PrettyConfig::new().struct_names(true));
+                let mut ser =
+                    ron::Serializer::with_options(buffer, config, ron::Options::default())?;
+                let ser = serde_path_to_error::Serializer::new(&mut ser, &mut track);
+
+                luminol_data::helpers::nil_padded_serde::serialize(data, ser).map_err(|inner| {
+                    let error = serde_path_to_error::Error::new(track.path(), inner);
+                    format_path_to_error(error)
+                })?;
+            }
+        }
 
         Ok(())
     }
--- crates/data/src/helpers/parameter_type.rs
+++ crates/data/src/helpers/parameter_type.rs
@@ -22,6 +22,8 @@
 // terms of the Steamworks API by Valve Corporation, the licensors of this
 // Program grant you additional permission to convey the resulting work.
 
+use alox_48::Value;
+
 use crate::rgss_structs::{Color, Tone};
 use crate::shared::{AudioFile, MoveCommand, MoveRoute};
 
@@ -36,7 +38,7 @@ pub enum ParameterType {
     Color(Color),
     Tone(Tone),
     AudioFile(AudioFile),
-    Float(f32),
+    Float(f64),
     MoveRoute(MoveRoute),
     MoveCommand(MoveCommand),
     Array(Vec<ParameterType>),
@@ -48,15 +50,46 @@ pub enum ParameterType {
 
 // FIXME this really should be try_from and try_into
 impl From<alox_48::Value> for ParameterType {
-    // This is a massive sore spot right now, so I'm not even bothering...
-    fn from(_value: alox_48::Value) -> Self {
-        ParameterType::None
+    fn from(value: alox_48::Value) -> Self {
+        match value {
+            Value::Nil => Self::None,
+            Value::Integer(v) => Self::Integer(v),
+            Value::Float(v) => Self::Float(v),
+            Value::String(v) => Self::String(String::from_utf8(v.data).unwrap()),
+            Value::Array(v) => Self::Array(v.into_iter().map(|v| v.into()).collect()),
+            Value::Bool(v) => Self::Bool(v),
+            Value::Userdata(userdata) => match userdata.class.as_str() {
+                "Color" => Self::Color(Color::from(userdata)),
+                "Tone" => Self::Tone(Tone::from(userdata)),
+                _ => panic!("Unsupported userdata type: {:#?}", userdata),
+            },
+            Value::Object(alox_48::Object { ref class, .. }) => match class.as_str() {
+                "RPG::AudioFile" => Self::AudioFile(alox_48::from_value(&value).unwrap()),
+                "RPG::MoveRoute" => Self::MoveRoute(alox_48::from_value(&value).unwrap()),
+                "RPG::MoveCommand" => Self::MoveCommand(alox_48::from_value(&value).unwrap()),
+                _ => panic!("Unsupported object type: {:#?}", value),
+            },
+            Value::Instance(i) => (*i.value).into(),
+            _ => panic!("Unsupported value type: {:#?}", value),
+        }
     }
 }
 
 impl From<ParameterType> for alox_48::Value {
-    fn from(_value: ParameterType) -> Self {
-        alox_48::Value::Nil
+    fn from(value: ParameterType) -> Self {
+        match value {
+            ParameterType::None => Value::Nil,
+            ParameterType::Integer(v) => Value::Integer(v),
+            ParameterType::Float(v) => Value::Float(v),
+            ParameterType::String(v) => Value::String(v.into()),
+            ParameterType::Array(v) => Value::Array(v.into_iter().map(|v| v.into()).collect()),
+            ParameterType::Bool(v) => Value::Bool(v),
+            ParameterType::Color(v) => Value::Userdata(v.into()),
+            ParameterType::Tone(v) => Value::Userdata(v.into()),
+            ParameterType::AudioFile(v) => alox_48::to_value(v).unwrap(),
+            ParameterType::MoveRoute(v) => alox_48::to_value(v).unwrap(),
+            ParameterType::MoveCommand(v) => alox_48::to_value(v).unwrap(),
+        }
     }
 }
 
@@ -153,7 +186,7 @@ variant_impl! {
     Color, Color,
     Tone, Tone,
     AudioFile, AudioFile,
-    Float, f32,
+    Float, f64,
     MoveRoute, MoveRoute,
     MoveCommand, MoveCommand,
     Array, Vec<ParameterType>,
