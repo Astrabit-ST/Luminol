981bda24c173864d584c2b4e13a7bc24bf035bca
刘皓 <whiteaxe@tuta.io>
2025-06-17 17:16:29 -0400

    Remove `name_vec_alox` and `name_vec_serde`

--- crates/data/src/helpers/mod.rs
+++ crates/data/src/helpers/mod.rs
@@ -17,12 +17,10 @@
 
 pub mod id_serde;
 pub mod id_vec_serde;
-pub mod name_vec_serde;
 pub mod nil_padded_serde;
 
 pub mod id_alox;
 pub mod id_vec_alox;
-pub mod name_vec_alox;
 pub mod nil_padded_alox;
 
 mod parameter_type;
